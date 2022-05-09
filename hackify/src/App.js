0dc239fbe9e5c846c5a6b21d3f0dd076d4c017b5
Manoel Castro <manoelcl@gmail.com>
2022-05-09 19:55:26 +0200

    deleted comments added set result

--- hackify/src/App.js
+++ hackify/src/App.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import "./components/SearchBar";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   const [result, setResult] = useState("");
   return (
     <div className="App">
-      <SearchBar handler={(string) => setResult(string)} />
+      <SearchBar handler={({ text }) => setResult(text)} />
       <div>{result}</div>
     </div>
   );
--- hackify/src/components/SearchBar.jsx
+++ hackify/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function SearchBar({ handler }) {
   const handleChange = (e) => {
-    handler(e.target.value);
+    handler({ text: e.target.value });
   };
   return (
     <input
